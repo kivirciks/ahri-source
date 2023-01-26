c1eb6cfd619b3fcaaad84ae3bb6ef83d5c101b63
zeio <zeionara@gmail.com>
2023-01-26 17:58:41 +0300

    Added labs descriptions from 3rd to 7th

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -70,7 +70,12 @@ module.exports = {
           collapsable: false,
           children: [
             'task/1',
-            'task/2'
+            'task/2',
+            'task/3',
+            'task/4',
+            'task/5',
+            'task/6',
+            'task/7'
           ]
         }
       ],
