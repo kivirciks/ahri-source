ede3c7cedf8130818f5a4ca28977b860ca18718f
zeio <zeionara@gmail.com>
2023-02-01 15:58:35 +0300

    Updated favicon path

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -20,7 +20,7 @@ module.exports = {
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-    ['link', { rel: 'icon', ref: 'ahri/favicon.ico' }]
+    ['link', { rel: 'icon', ref: '/ahri/favicon.ico' }]
   ],
 
   /**
